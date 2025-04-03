bb43993924a1c87055fc9cb6b66a553e1bdb27b2
Francois Piumi <francois.piumi@inrae.fr>
2025-04-03 21:59:33 +0200

    6.minimap2_coding_noncoding_transcriptome.sh

--- 6.minimap2_coding_noncoding_transcriptome.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-module load bioinfo/Minimap/2-2.26
-
-minimap2 -ax map-ont -N 100 \
-reference.cdna.ncrna.wo.duplicatesID.fa \ 
-sequences.fastq.gz > minimap2_coding_noncoding_transcriptome.sam
